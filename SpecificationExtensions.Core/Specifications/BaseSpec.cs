1cc10858ee0931245e084aa3b32134b3ccedecbb
Taras Polisnichenko <taras_polisnichenko@epam.com>
2022-08-06 13:36:28 +0300

    Fixed formatting

--- SpecificationExtensions.Core/Specifications/BaseSpec.cs
+++ SpecificationExtensions.Core/Specifications/BaseSpec.cs
@@ -18,36 +18,37 @@ namespace SpecificationExtensions.Core.Specifications
         }
 
         public BaseSpec<TModel> ExceptBy<TKey>(IEnumerable<TKey> values, Expression<Func<TModel, TKey>> selector)
+        {
+            foreach (var value in values)
             {
-                foreach (var value in values)
-                {
-                    var lambda = GetNotEqualExpression(selector, value);
-        
-                    Query
-                        .Where(lambda);
-                }
-                
-                return this;
+                var lambda = GetNotEqualExpression(selector, value);
+
+                Query
+                    .Where(lambda);
             }
-            
-            public BaseSpec<TModel> ExceptBy<TKey>(IEnumerable<TModel> models, Expression<Func<TModel, TKey>> selector)
-            {
-                var compiledSelector = selector.Compile();
-                var values = models.Select(compiledSelector);
-        
-                ExceptBy(values, selector);
-                
-                return this;
-            }
-        
-            private Expression<Func<TModel, bool>> GetNotEqualExpression<TKey>(Expression<Func<TModel, TKey>> predicate, TKey value)
-            {
-                var parameterExpression = predicate.Parameters.Single();
-                var constantExpression = Expression.Constant(value);
-                var notEqualExpression = Expression.NotEqual(predicate.Body, constantExpression);
-                var lambda = Expression.Lambda<Func<TModel, bool>>(notEqualExpression, parameterExpression);
-                
-                return lambda;
-            }
+
+            return this;
+        }
+
+        public BaseSpec<TModel> ExceptBy<TKey>(IEnumerable<TModel> models, Expression<Func<TModel, TKey>> selector)
+        {
+            var compiledSelector = selector.Compile();
+            var values = models.Select(compiledSelector);
+
+            ExceptBy(values, selector);
+
+            return this;
+        }
+
+        private Expression<Func<TModel, bool>> GetNotEqualExpression<TKey>(
+            Expression<Func<TModel, TKey>> predicate, TKey value)
+        {
+            var parameterExpression = predicate.Parameters.Single();
+            var constantExpression = Expression.Constant(value);
+            var notEqualExpression = Expression.NotEqual(predicate.Body, constantExpression);
+            var lambda = Expression.Lambda<Func<TModel, bool>>(notEqualExpression, parameterExpression);
+
+            return lambda;
+        }
     }
 }
